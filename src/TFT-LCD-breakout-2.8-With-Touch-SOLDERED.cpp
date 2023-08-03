d7d7a7e2e040bb97ad64b6e3743cc02a9609276c
Main Push Robot <robot@example.com>
2023-08-03 12:39:19 +0000

    Committing dev changes

--- src/TFT-LCD-breakout-2.8-With-Touch-SOLDERED.cpp
+++ src/TFT-LCD-breakout-2.8-With-Touch-SOLDERED.cpp
@@ -9,24 +9,4 @@
  * @authors     @ soldered.com
  ***************************************************/
 
-
 #include "TFT-LCD-breakout-2.8-With-Touch-SOLDERED.h"
-
-/**
- * @brief                   Sensor specific native constructor.
- *
- * @param int _pin          Example parameter.
- */
-Sensor::Sensor(int _pin)
-{
-    pin = _pin;
-    native = 1;
-}
-
-/**
- * @brief                   Overloaded function for virtual in base class to initialize sensor specific.
- */
-void Sensor::initializeNative()
-{
-    pinMode(pin, INPUT);
-}
--- src/libs/Adafruit_ILI9341_SR/examples/graphicstest/graphicstest.ino
+++ src/libs/Adafruit_ILI9341_SR/examples/graphicstest/graphicstest.ino
@@ -1,35 +1,49 @@
-/**
- **************************************************
- *
- * @file        Template.ino
- * @brief       Example template for easyC sensors
- *
- *
- *
- * @authors     @ soldered.com
- ***************************************************/
-#include <SPI.h>
-#include "TFT-LCD-breakout-2.8-With-Touch-SOLDERED.h"
+/***************************************************
+  This is our GFX example for the Adafruit ILI9341 Breakout and Shield
+  ----> http://www.adafruit.com/products/1651
 
-// cs 8
-// rst 6
-// rs 7
-// wr 4
-// rd 5
-SPIClass SPI1;
-Adafruit_ILI9341 tft(&SPI1, 8, 7, 4, 5, 6);
+  Check out the links above for our tutorials and wiring diagrams
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional)
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+// For the Adafruit shield, these are the default.
+#define TFT_DC 9
+#define TFT_CS 10
+
+// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+// If using the breakout, change pins as desired
+//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 void setup() {
-    delay(1000);
   Serial.begin(115200);
   Serial.println("ILI9341 Test!"); 
-    delay(1000);
-    delay(1000);
-    delay(1000);
  
   tft.begin();
 
-  Serial.println("After tft begin!"); 
+  // read diagnostics (optional but can help debug problems)
+  uint8_t x = tft.readcommand8(ILI9341_RDMODE);
+  Serial.print("Display Power Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDMADCTL);
+  Serial.print("MADCTL Mode: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDPIXFMT);
+  Serial.print("Pixel Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDIMGFMT);
+  Serial.print("Image Format: 0x"); Serial.println(x, HEX);
+  x = tft.readcommand8(ILI9341_RDSELFDIAG);
+  Serial.print("Self Diagnostic: 0x"); Serial.println(x, HEX); 
   
   Serial.println(F("Benchmark                Time (microseconds)"));
   delay(10);
